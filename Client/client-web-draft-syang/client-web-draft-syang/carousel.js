97cc2713e5be0056ef0f597e3e402b0f424218c0
Seng Xai Yang <yangsx7908@uwec.edu>
2021-07-05 20:38:22 -0500

    New add

---
+++ Client/client-web-draft-syang/client-web-draft-syang/carousel.js
@@ -0,0 +1,38 @@
+/*$('.carousel .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});*/
+
+$(document).ready(function(){
+
+    $('#myCarousel').carousel({ interval: 3000 });
+    
+    $('.multi-item-carousel .item').each(function(){
+    var itemToClone = $(this);
+    
+    for (var i=1;i<4;i++) {
+    itemToClone = itemToClone.next();
+    
+    if (!itemToClone.length) {
+    itemToClone = $(this).siblings(':first');
+    }
+    
+    itemToClone.children(':first-child').clone()
+    .addClass("cloneditem-"+(i))
+    .appendTo($(this));
+    }
+    });
+    });
